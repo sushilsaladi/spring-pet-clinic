bba1ab3e851425f553d5f9c002c085004fa4a184
sushilsaladi <sushilsaladi@Sushils-MacBook-Pro.local>
2023-09-26 11:47:07 +0530

    pet-clinic poc

---
+++ src/test/java/saladi/findingspring/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package saladi.findingspring.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
